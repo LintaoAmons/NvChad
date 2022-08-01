a35347f64b9aac51171be9fff8ab8492b6b1aa1f
Lintao <Lintao.Amons@outlook.com>
2022-08-01 14:34:35 +0800

    add find group mapping

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -20,6 +20,23 @@ M.Format = {
   }
 }
 
+M.Find = {
+  n = {
+    ["<leader>fb"] = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+		["<leader>fc"] = { "<cmd>Telescope commands<cr>", "Commands" },
+		["<leader>fd"] = { "<cmd>Telescope diagnostics<cr>", "Find lsp diagnostics" },
+		["<leader>fe"] = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+		["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+		["<leader>fl"] = { "<cmd>NvimTreeFindFile<cr>", "Find file's Location" },
+		["<leader>fM"] = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+		["<leader>fR"] = { "<cmd>Telescope registers<cr>", "Registers" },
+    ["<leader>fs"] = { "<cmd>Telescope lsp_document_symbols<CR>", "Find Symbols"},
+		["<leader>ff"] = { "<cmd>Telescope live_grep<cr>", "Text" },
+		["<leader>fk"] = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+		["<leader>fp"] = { "<cmd>Telescope projects<cr>", "Find Recent Projects" },
+  }
+}
+
 M.Quit = {
   n = {
     ["<leader>qq"] = { "<cmd>qa!<cr>", "Quit vim" }
