745ef097fc81769ae1c7ccd8025f9680db1da055
Lintao <Lintao.Amons@outlook.com>
2022-08-02 16:35:31 +0800

    remove auto-save plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -10,13 +10,13 @@ return {
       require("custom.plugins.null-ls").setup()
     end,
   },
-  ["Pocco81/auto-save.nvim"] = {
-    commit = "268069b1a5fab571e33f6c8de95e45f0e52423db",
-    config = function()
-      require("auto-save").setup {
-        -- your config goes here
-        -- or just leave it empty :)
-      }
-    end,
-  },
+  -- ["Pocco81/auto-save.nvim"] = {
+  --   commit = "268069b1a5fab571e33f6c8de95e45f0e52423db",
+  --   config = function()
+  --     require("auto-save").setup {
+  --       -- your config goes here
+  --       -- or just leave it empty :)
+  --     }
+  --   end,
+  -- },
 }
